8cfae36d5675295af008876e055adbc1df57ad1b
Niganti Thiprungsee <n-ual2511@hotmail.com>
2021-12-12 16:15:25 +0700

    Intention to commit

--- lib/test.dart
+++ lib/test.dart
@@ -2,21 +2,36 @@
 import 'dart:io';
 import 'dart:math';
 
-void main(){
-  var i =0;
+void main() {
+  var i = 0;
   var r = Random();
-  var answer = r.nextInt(10);
+  var answer = r.nextInt(100);
+  int count =0;
+  print('╔═════════════════════════════════════════════════════');
+  print('║                    GUESS THE NUMBER');
+  print('║─────────────────────────────────────────────────────');
 
-  while(true) {
-    //print('kk'); ขึ้นบรรทัดใหม่
-    stdout.write('Please guess the number : '); //ไม่ขึ้นบรรทัดใหม่
+  while (true) {
+    stdout.write('║ Guess the number between 1 and 100 : '); //ไม่ขึ้นบรรทัดใหม
     var input = stdin.readLineSync();
     var guess = int.tryParse(input!);
-    if(guess == answer){
-      print('Correct!!! Answer is '+ input);
-      break;
-    }else{
-      print('Incorrect!!! Please try again');
+    if (guess != null) {
+      if (guess == answer) {
+        count++;
+        print('║ ➜ '+guess.toString()+' is CORRECT ❤ , total guesses : ' + count.toString());
+        print('║─────────────────────────────────────────────────────');
+        print('║                      THE END');
+        print('╚═════════════════════════════════════════════════════');
+        break;
+      } else if(guess > answer){
+        print('║ ➜ '+guess.toString()+' IS TOO HIGTH! ▲');
+        print('║─────────────────────────────────────────────────────');
+        count++;
+      }else if(guess < answer){
+        count++;
+        print('║ ➜ '+guess.toString()+' IS TOO LOW! ▼');
+        print('║─────────────────────────────────────────────────────');
+      }
     }
   }
 }
